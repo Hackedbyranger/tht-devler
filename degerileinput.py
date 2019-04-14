18fedde67ef9afb28a524327da2a009fb6da97be
Hackedbyranger <41688107+Hackedbyranger@users.noreply.github.com>
2019-04-14 13:32:38 +0300

    Add files via upload

---
+++ degerileinput.py
@@ -0,0 +1,64 @@
+print("RANGER İŞLEMLER HER BİRİ DİĞERİ BİTTİKÇE SIRAYLA GELECEKTİR.")
+
+
+#toplama
+
+print(" Ranger  input ile değer alarak toplama işlemi ")
+
+print("Bir Sayı Seçiniz:")
+
+
+
+toplanan=int(input("1.ci sayi: "))
+toplanan2=int (input("2.ci sayiyi yazınız: "))
+toplam= int(toplanan+toplanan2)
+# buradaki + işareti toplama işi görür. "- çıkartma " ,"* çarpma", "/ bölme" olarak değişebilir //RANGER
+
+print(toplam)
+
+
+#Çarpma
+
+print(" Ranger  input ile değer alarak ÇARPMA işlemi ")
+print("Bir Sayı Seçiniz:")
+
+
+carpılan=int(input("1.ci sayi: "))
+carpılan2=int (input("2.ci sayiyi yazınız: "))
+carp= int(carpılan*carpılan2)
+print(carp)
+
+print(" Ranger  input ile değer alarak BÖLME işlemi ")
+
+#bölme
+
+print("Bir Sayı Seçiniz:")
+
+
+
+bolunen=int(input("1.ci sayi: "))
+bolunen2=int (input("2.ci sayiyi yazınız: "))
+bol= int(bolunen/bolunen2)
+
+
+print(bol)
+
+
+#Çıkartma
+
+print(" Ranger  input ile değer alarak ÇIKARTMA işlemi ")
+
+print("Bir Sayı Seçiniz:")
+
+
+
+cıkan=int(input("1.ci sayi: "))
+cıkan2=int (input("2.ci sayiyi yazınız: "))
+cık= int(cıkan-cıkan2)
+
+
+print(cık)
+
+
+
+input("İncelediğiniz için teşekkür ederim. Çıkmak için herhangi bir tuşa basabilirsiniz.//Ranger")
---
+++ toplamacıkarmavs.py
@@ -0,0 +1,20 @@
+print("Ranger fonksiyonlar")
+
+a= 4
+b= 8
+
+print("TOPLAMA:")
+print (a+b)
+
+print("Çarpma:")
+print(a*b)
+
+
+print("Çıkartma")
+print(b-a)
+
+
+print("Bolme")
+print(b/a)
+
+input("Bu input program kapanmaması için konulmuştur (RANGER)")
